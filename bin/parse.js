e965b5a0eb0e2c5442eb7a4c7202df8649e9c7a5
will Farrell <will.farrell@gmail.com>
2018-02-15 15:30:16 -0700

    init commit

---
+++ bin/parse.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const convert = require('xml-js');
+
+console.log('Parsing WQX All Domain Values XML ...');
+
+// download and unzip http://cdx.epa.gov/wqx/download/DomainValues/All.zip
+// parse xml for allowed values schemas
+var xml = require('fs').readFileSync(__dirname+'/../xml/All Domain Values.xml', 'utf8');
+const values = JSON.parse(convert.xml2json(xml, {compact: true}));
+
+const jsonSchema = {
+    title:'Allowed Values',
+    description:'WQX Allowed Values Definitions',
+    // Missing from file, subsets of MeasureUnit
+    AreaMeasurementUnit: {
+        title: 'Area Measurement Unit',
+        description: '',
+        type: 'string',
+        enum: ['cm3','in3','ft3', 'm3']
+    },
+    DistanceMeasurementUnit: {
+        title: 'Distance Measurement Unit',
+        description: '',
+        type: 'string',
+        enum: ['cm','in','ft', 'm', 'km']
+    },
+    TimeMeasurementUnit: {
+        title: 'Time Measurement Unit',
+        description: '',
+        type: 'string',
+        enum: ['seconds','minutes','hours','weeks','months','years']
+    },
+    SpeedMeasurementUnit: {
+        title: 'Speed Measurement Unit',
+        description: '',
+        type: 'string',
+        enum: ['cm/sec','ft/sec','m/sec','km/sec','km/hr']
+    },
+    MassMeasurementUnit: {
+        title: 'Mass Measurement Unit',
+        description: '',
+        type: 'string',
+        enum: ['g','oz','lb','kg']
+    }
+};
+for (let e in values.WQXDomainValueList.WQXElement) {
+    const field = values.WQXDomainValueList.WQXElement[e].WQXElementName._text.replace('\n', '').trim();
+    const element = values.WQXDomainValueList.WQXElement[e].WQXElementRow;
+
+    jsonSchema[field] = {
+        title: field,
+        description:'',
+        type:'string',
+        enum:[]
+    };
+    for (let r in element) {
+        const row = element[r].WQXElementRowColumn;
+        const rowObj = {};
+
+        // re-org
+        for (let c in row) {
+            const col = row[c]._attributes;
+            rowObj[col.colname] = col.value;
+        }
+
+        let value = '';
+        if (Object.keys(rowObj).indexOf('ID') !== -1) {
+            value = rowObj['ID'];
+        } else if (Object.keys(rowObj).indexOf('Characteristic') !== -1) {  // CharacteristicPickListValue
+            value = rowObj['Characteristic'];
+        } else if (Object.keys(rowObj).indexOf('Code') !== -1) {
+            value = rowObj['Code'];
+        } else if (Object.keys(rowObj).indexOf('CountyFIPSCode') !== -1) {  // County
+            value = rowObj['CountyName']+', '+rowObj['StateCode'];
+        } else {
+            value = rowObj['Name'];
+        }
+
+        // meta data for docs
+        // name = rowObj['Name'].replace(/( ?[A-Z])/g, ' $1');
+        // description = rowObj['Description']
+
+        jsonSchema[field].enum.push(value);
+    }
+}
+
+fs.writeFileSync(__dirname+'/../src/definitions.wqx.values.json', JSON.stringify(jsonSchema, null, 2), 'utf8');
+console.log('Done!');
---
+++ docs/README.md
@@ -0,0 +1,74 @@
+<h1 align="center">
+  <img src="https://raw.githubusercontent.com/gordonfn/datastream-wqx/master/docs/images/water-quality-exchange.gif" alt="WQX Logo" width="200">
+  <br>
+  Water Quality Exchange (WQX): JSON Schema and JSON Table Schema
+  <br>
+  <br>
+</h1>
+
+<p align="center">
+  <a href="https://github.com/gordonfn/wqx"><img src="https://img.shields.io/github/stars/gordonfn/wqx.svg?style=social&label=Stars" alt="Stars" /></a>
+  <a href="https://www.npmjs.com/package/wqx"><img src="https://img.shields.io/npm/v/wqx.svg" alt="npm version"></a>
+  <a href="https://www.npmjs.com/package/wqx"><img src="https://img.shields.io/npm/dm/wqx.svg" alt="npm downloads"></a>
+  <a href="https://www.npmjs.com/package/wqx"><img src="https://img.shields.io/npm/l/wqx.svg" alt="npm license" /></a>
+  <a href="http://packagequality.com/#?package=wqx"><img src="http://npm.packagequality.com/shield/wqx.svg" alt="Package Quality" /></a>
+</p>
+
+## Sponsored By
+[![The Gordon Foundation](https://raw.githubusercontent.com/gordonfn/datastream-wqx/master/docs/images/the-gordon-foundation.svg)](http://gordonfoundation.ca)
+
+## Install
+```bash
+$ npm i wqx
+```
+
+## Schemas
+- `project`
+- `location`
+- `activitymetric`
+- `biological`
+- `biological-habitat`
+- `habitat`
+- `physical-chemistry`
+
+### JSON Schema
+
+### JSON Table Schema
+
+## Use
+```javascript
+const Ajv = require('ajv');
+const jsonschema = requrie('wqx/json-schema/biological.json');
+
+
+```
+
+
+## Contributing
+
+### Building `definitions.wqx.values.json`
+```bash
+$ npm run build:values
+```
+
+### Maintained By
+[![Tesera](https://raw.githubusercontent.com/gordonfn/datastream-wqx/master/docs/images/tesera.png)](https://tesera.com)
+
+
+### Contributors
+- [willfarrell](https://github.com/willfarrell)
+
+## References
+- [About](https://www3.epa.gov/storet/archive/web/wqx.html)
+- [Web Template Files](https://www.epa.gov/waterdata/water-quality-exchange-web-template-files)
+- [Schema (XML)](http://www.exchangenetwork.net/data-exchange/wqx/)
+- [Schema Allowed Values (XML)](http://www.epa.gov/storet/wqx/wqx_getdomainvalueswebservice.html)
+
+
+## TODO
+- [ ] script to generate docs
+  - [ ] ability to search allowed values
+- [ ] wqx schemas should be namespaced in npm
+- [ ] add file level definitions to help minimize size
+- [ ] script to build json-schema
+- [ ] script to build json-table-schema
